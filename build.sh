51f403e5064c3ca627b69c7d8113050859a2f06d
g0lem <sasteve999@gmail.com>
2023-06-02 10:16:53 +0300

    added stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # gcc main.c && ./a.out
-
+source ./emsdk/emsdk_env.sh
 #OpenGL
 clear
 emcc ./src/main.c -s MIN_WEBGL_VERSION=2 -s ASYNCIFY -s USE_GLFW=3 -o server/public/main.html --embed-file src/assets
--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NULL 0
-
 #define GLFW_INCLUDE_ES2
 #include "renderer/renderer.h"
 
